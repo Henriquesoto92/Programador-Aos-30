f64c16b5fb0bffe44698921d586358a6432848c7
Henrique Soto <96528794+Henriquesoto92@users.noreply.github.com>
2023-06-28 17:25:11 -0300

    :lipstick: Add new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      colors: {
+        primary: {
+          light: "#ffffff",
+          default: "#ECF3FE",
+          blue: {
+            1: "#122447",
+            2: "#ECF3FE",
+            3: "#518AE1",
+            4: "#004D8C",
+            5: "#7BA0D8",
+          },
+          yellow: {
+            1: "#F6AB0B",
+          },
+          dark: "#111111",
+        },
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        // "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        // "gradient-conic":
+        //   "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
+};
