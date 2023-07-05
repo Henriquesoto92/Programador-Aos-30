0e5bbd63470c75d0343460cec2dbd1e7c55a4b76
henriquesoto92 <henriquesoto92@gmail.com>
2023-07-04 22:06:10 -0300

    :iphone: Working in responsive navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -46,7 +46,31 @@ const socialLinks: ISocialLinks[] = [
 const Navbar = () => {
   const pathname = usePathname();
   return (
-    <div className="h-30 fixed left-0 top-0 flex w-full p-4 justify-between items-center bg-primary-light2/80">
+    <nav className="h-30 fixed left-0 top-0 flex w-full p-4 justify-between items-center bg-primary-light2/80">
+      <button
+        data-collapse-toggle="navbar-default"
+        type="button"
+        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        aria-controls="navbar-default"
+        aria-expanded="false"
+      >
+        <span className="sr-only">Open main menu</span>
+        <svg
+          className="w-5 h-5"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 17 14"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M1 1h15M1 7h15M1 13h15"
+          />
+        </svg>
+      </button>
       <div className="flex mr-[99px]">
         <Image
           src="/assets/logo/logo1.svg"
@@ -55,7 +79,8 @@ const Navbar = () => {
           height={56}
         />
       </div>
-      <ul className="flex gap-5">
+
+      <ul className="hidden md:flex gap-5">
         {pathLinks.map((item, index) => {
           const isActive = pathname.endsWith(item.href);
 
@@ -76,10 +101,8 @@ const Navbar = () => {
           );
         })}
       </ul>
-      <div className="flex gap-8">
+      <div className="hidden md:flex gap-8 w-full md:w-auto">
         {socialLinks.map((item, index) => {
-          const isActive = pathname.endsWith(item.href);
-
           return (
             <a href={item.href} key={`${index}-${item.name}`}>
               <Image src={item.src} alt="logo" width={32} height={32} />
@@ -87,7 +110,7 @@ const Navbar = () => {
           );
         })}
       </div>
-    </div>
+    </nav>
   );
 };
 
